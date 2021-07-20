e13bdf58296083e92e87f2a89056f2992b2f2544
michele donato <michele.donato@freenome.com>
2021-07-20 15:21:01 -0700

    set single py version for test

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.7"])
+@nox.session(python=["3.8"])# could list more than one python versions
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
