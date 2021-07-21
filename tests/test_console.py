65cbb38410e75d33a2a27a921140dd9199f213f4
michele donato <michele.donato@freenome.com>
2021-07-20 17:49:34 -0700

    applied suggestions from lint

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,12 +1,13 @@
 # tests/test_console.py
-from unittest.mock import Mock
+# from unittest.mock import Mock
 
 import click.testing
 import pytest
 import requests
 
-#idiota ricordati che hai cambiato nome al pacchetto
+# idiota ricordati che hai cambiato nome al pacchetto
 from hm_python_ttrl import console
+
 
 @pytest.fixture
 def mock_requests_get(mocker):
@@ -18,18 +19,22 @@ def mock_requests_get(mocker):
     # DO NOT FUCKING FORGET TO RETURN MOCK
     return mock
 
+
 @pytest.fixture
 def runner():
     return click.testing.CliRunner()
+
 
 def test_main_succeeds(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
 
+
 def test_main_prints_title(runner, mock_requests_get):
     result = runner.invoke(console.main)
     print(result.output)
     assert "Lorem Ipsum" in result.output
+
 
 # check what were the parameters passed to the request
 def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
@@ -37,11 +42,13 @@ def test_main_uses_en_wikipedia_org(runner, mock_requests_get):
     args, _ = mock_requests_get.call_args
     assert "en.wikipedia.org" in args[0]
 
+
 # Simulate an error on the request
 def test_main_fails_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
+
 
 # without modifying the wikipedia.py as well this won't work
 # because click does not deal with it (doesn't raise it?)
